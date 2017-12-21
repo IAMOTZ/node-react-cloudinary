230f2ee0780c36c8b58019e1d121dbe217437d79
tunmise <ogunniyitunmise@gmail.com>
2017-12-21 15:00:02 +0100

    Add a webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: [
+    path.join(__dirname, '/client/app.jsx'),
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015'],
+          plugins: ['transform-class-properties']
+        },
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, 'client'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'client')
+  },
+};
